ce044377250a746e7fd8d5da4ff841784891240d
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-08-25 00:51:04 -0700

    Added footer & modified product display

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Flower Shop</title>
+    <title>HS flowers</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
