0ba95aff589c38e4cfbf15d0e4e30eab5aee9c03
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-08-25 02:55:34 -0700

    main page demo

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -3,13 +3,14 @@
 @section('content')
 
     <div>
-        <div class="jumbotron jumbotron-fluid">
+        <div class="jumbotron jumbotron-fluid main_welcome">
             <div class="container">
-                <h1 class="display-3">This is my store, this is my flowers</h1>
-                <p class=" text-center lead">There is nothing more beautiful than my honi, in second place, its flowers.</p>
+                <p class=" text-center lead main_text_small"> Nothing more beautiful than my honi</p>
+                <h1 class="display-3 text-center main_text_large">PERFECT GIFT</h1>
+                <p class=" text-center lead main_text_small">beautiful than honi</p>
                 <div class="text-center">
                     <a href="{{url('shop/categories')}}">
-                        <button type="button" class="btn btn-outline-secondary">See Categories <i></i></button>
+                        <button type="button" class="btn btn-secondary main_welcome_btn">See Categories</button>
                     </a>
                 </div>
             </div>
@@ -56,61 +57,38 @@
         <div class="container mt-5 mb-4 text-center text-md-left">
             <div class="row mt-3">
                 <!--First column-->
-                <div class="col-md-3 col-lg-4 col-xl-6 mb-r dark-grey-text">
+                <div class="col-md-6 col-lg-6 col-xl-6 mb-r dark-grey-text">
                     <h6 class="title font-bold"><strong>Hannah & Samuel Flower Shop</strong></h6>
-                    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+                    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto footer_title_accent">
                     <p>
                         We are a full service florists providing Flower & Gift Delivery to the Vancouver area over 18 years.
                         Send flowers for any occasion. We offer a large variety of fresh flowers and gifts.
                         <br />Enjoy the convenience of safe and secure ordering online 24 hours a day.
                     </p>
                 </div>
-                <!--/.First column-->
+                <!--First column-->
 
                 <!--Second column-->
-                <div class="col-md-4 col-lg-3 col-xl-3 dark-grey-text">
+                <div class="col-md-6 col-lg-6 col-xl-6 dark-grey-text footer_second_column">
                     <h6 class="title font-bold"><strong>Location</strong></h6>
-                    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-                    <p><i class="fa fa-home mr-3"></i> 2130 Western Pky, Vancouver, BC V6T 1V6</p>
-                    <p><i class="fa fa-envelope mr-3"></i> hsflowers@gmail.com</p>
-                    <p><i class="fa fa-phone mr-3"></i> + 01 604-228-0113</p>
+                    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto footer_title_accent">
+                    <p><i class="fa fa-home mr-2"></i> 2130 Western Pky, Vancouver, BC V6T 1V6</p>
+                    <p><i class="fa fa-envelope mr-2"></i> <a href="mailto:hsflowers@gmail.com">Send Email</a> </p>
+                    <p><i class="fa fa-phone mr-2"></i> + 01 604-228-0113</p>
                 </div>
-                <!--/.Second column-->
-
-                <!--Third column-->
-                {{--<div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-r dark-grey-text">--}}
-                    {{--<h6 class="title font-bold"><strong>Useful links</strong></h6>--}}
-                    {{--<hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">--}}
-                    {{--<p><a href="#!" class="dark-grey-text">Your Account</a></p>--}}
-                    {{--<p><a href="#!" class="dark-grey-text">Become an Affiliate</a></p>--}}
-                    {{--<p><a href="#!" class="dark-grey-text">Shipping Rates</a></p>--}}
-                    {{--<p><a href="#!" class="dark-grey-text">Help</a></p>--}}
-                {{--</div>--}}
-                <!--/.Third column-->
-
-                <!--Fourth column-->
-
-                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-r dark-grey-text">
-                    <h6 class="title font-bold"><strong>Categories</strong></h6>
-                    <hr class="teal accent-3 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
-                    <p><a href="#!" class="dark-grey-text">Any Occasion</a></p>
-                    <p><a href="#!" class="dark-grey-text">Love & Romance</a></p>
-                    <p><a href="#!" class="dark-grey-text">Sympathy</a></p>
-                    <p><a href="#!" class="dark-grey-text">Office Plants</a></p>
-                </div>
-                <!--/.Fourth column-->
-
+                <!--Second column-->
             </div>
+            <!-- row mt-3 -->
         </div>
-        <!--/.Footer Links-->
-
+        <!-- container -->
+        <!--Footer Links-->
         <!-- Copyright-->
         <div class="footer-copyright">
             <div class="container-fluid">
-                © 2017 Copyright Hannah & Samuel Flower Shop</strong></a>
+                © 2017 Hannah & Samuel Flower Shop. Developed by <a target="_blank" href="http://timoshockness.com">Timoteo</a> and <a target="_blank" href="http://kateyeeum.com">Kate</a>
             </div>
         </div>
-        <!--/.Copyright -->
+        <!--Copyright -->
 
     </footer>
     <!--/.Footer-->
