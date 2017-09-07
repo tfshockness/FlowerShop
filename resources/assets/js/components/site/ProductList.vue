26ea6ee8182378b848d3e7071bc087c0cc4c2397
yiyeum <KateYeEumLee@iyeeums-MacBook-Air.local>
2017-09-07 01:26:51 -0700

    to display 3 items in a row

--- resources/assets/js/components/site/ProductList.vue
+++ resources/assets/js/components/site/ProductList.vue
@@ -1,25 +1,24 @@
 <template>
-    <div>
-        <div class="row">
-            <div class="col-md-3 text-center">
-                <a :href="link">
-                    <img src="https://demo.woothemes.com/storefront/wp-content/plugins/woocommerce/assets/images/placeholder.png" alt="product image" height="300" width="230">
-                </a>
-                <div class="text-center">
-                    <p>CAD {{product.price}}</p>
-                    <p>{{product.name}} <button href="#">Add to cart</button></p>
+    <div class="col-md-4 text-center">
 
-                </div>
-            </div>
+        <a :href="link">
+            <img src="https://demo.woothemes.com/storefront/wp-content/plugins/woocommerce/assets/images/placeholder.png"
+                 alt="product image" height="300" width="230">
+        </a>
+        <div class="text-center">
+            <p class="display_product_name">{{product.name}}</p>
+            <p class="display_product_price">CAD {{product.price}}</p>
+            <button type="button" class="btn btn-sm add_to_cart_btn" href="#">Add to cart</button>
         </div>
+
     </div>
 </template>
 
 <script>
     export default {
-        props:['product'],
-        computed:{
-            link: function(){
+        props: ['product'],
+        computed: {
+            link: function () {
                 return '/shop/' + this.product.slug
             }
         }
@@ -28,5 +27,4 @@
 </script>
 
 <style>
-
 </style>
